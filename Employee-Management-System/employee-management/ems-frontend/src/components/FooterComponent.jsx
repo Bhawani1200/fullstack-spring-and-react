16f0d08717b8ad2965524e46ae355575a2c70ab3
Bhawani1200 <bhawanilimbu1200@gmail.com>
2025-02-03 14:27:30 +0545

    employee management system

---
+++ Employee-Management-System/employee-management/ems-frontend/src/components/FooterComponent.jsx
@@ -0,0 +1,11 @@
+const FooterComponent = () => {
+  return (
+    <div>
+      <footer className="footer mb-0">
+        <span>All rights reserved by javaguides 2024</span>
+      </footer>
+    </div>
+  );
+};
+
+export default FooterComponent;
