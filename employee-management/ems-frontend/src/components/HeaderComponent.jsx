7a13d17854fa05a5e4c2327f6b868bb219f82be8
Bhawani1200 <bhawanilimbu1200@gmail.com>
2025-02-04 19:36:33 +0545

    changes made

---
+++ employee-management/ems-frontend/src/components/HeaderComponent.jsx
@@ -0,0 +1,13 @@
+const HeaderComponent = () => {
+  return (
+    <div>
+      <header className="navbar navbar-dark bg-dark">
+        <a className="navbar-brand" href="#">
+          Employee Management System
+        </a>
+      </header>
+    </div>
+  );
+};
+
+export default HeaderComponent;
