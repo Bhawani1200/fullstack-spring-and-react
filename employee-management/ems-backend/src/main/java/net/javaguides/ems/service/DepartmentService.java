bc84410533ef7932101396d6c8d7dcc4ee86e5fd
Bhawani1200 <bhawanilimbu1200@gmail.com>
2025-02-06 15:41:50 +0545

    create and get REST API

---
+++ employee-management/ems-backend/src/main/java/net/javaguides/ems/service/DepartmentService.java
@@ -0,0 +1,9 @@
+package net.javaguides.ems.service;
+
+import net.javaguides.ems.dto.DepartmentDto;
+
+public interface DepartmentService {
+    DepartmentDto createDepartment(DepartmentDto departmentDto);
+
+    DepartmentDto getDepartmentBtId(Long departmentId);
+}
---
+++ employee-management/ems-backend/src/main/java/net/javaguides/ems/service/impl/DepartmentServiceImpl.java
@@ -0,0 +1,34 @@
+package net.javaguides.ems.service.impl;
+
+import lombok.AllArgsConstructor;
+import net.javaguides.ems.dto.DepartmentDto;
+import net.javaguides.ems.entity.Department;
+import net.javaguides.ems.exception.ResourceNotFoundException;
+import net.javaguides.ems.mapper.DepartmentMapper;
+import net.javaguides.ems.repository.DepartmentRepository;
+import net.javaguides.ems.service.DepartmentService;
+import org.springframework.stereotype.Service;
+@Service
+@AllArgsConstructor
+public class DepartmentServiceImpl implements DepartmentService {
+
+    private DepartmentRepository departmentRepository;
+
+    @Override
+    public DepartmentDto createDepartment(DepartmentDto departmentDto) {
+        Department department = DepartmentMapper.mapToDepartment(departmentDto);
+        Department savedDepartment = departmentRepository.save(department);
+        return DepartmentMapper.mapToDepartmentDto(savedDepartment);
+
+    }
+
+    @Override
+    public DepartmentDto getDepartmentBtId(Long departmentId) {
+        Department department = departmentRepository.findById(departmentId).orElseThrow(
+                () -> new ResourceNotFoundException("Department is not exists with a given id: " + departmentId)
+        );
+        return DepartmentMapper.mapToDepartmentDto(department);
+    }
+
+
+}
