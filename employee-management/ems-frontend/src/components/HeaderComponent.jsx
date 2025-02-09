fde4d452b630b57245ab0e2a0c4bee750fe12d41
Bhawani1200 <bhawanilimbu1200@gmail.com>
2025-02-09 13:54:47 +0545

    working on department section

--- employee-management/ems-frontend/src/components/HeaderComponent.jsx
+++ employee-management/ems-frontend/src/components/HeaderComponent.jsx
@@ -1,10 +1,25 @@
+import { NavLink } from "react-router-dom";
+
 const HeaderComponent = () => {
   return (
     <div>
-      <header className="navbar navbar-dark bg-dark">
-        <a className="navbar-brand" href="#">
-          Employee Management System
-        </a>
+      <header>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <a className="navbar-brand" href="#">
+            Employee Management System
+          </a>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <NavLink className='nav-item' to="/employees">Employees</NavLink>
+              </li>
+              
+              <li className="nav-item">
+                <NavLink className='nav-item' to="/departments">Departments</NavLink>
+              </li>
+            </ul>
+          </div>
+        </nav>
       </header>
     </div>
   );
