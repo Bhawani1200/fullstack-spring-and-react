cf5f591588e25d51637c811dd83a484499ea1d77
Bhawani1200 <bhawanilimbu1200@gmail.com>
2025-02-28 14:04:03 +0545

    getodo with modelmapper

--- Routine-Management/Routine-to-do-management/src/main/java/GurkhaYouthsTrainingCenter/Routine_to_do_management/service/TodoService.java
+++
@@ -1,7 +0,0 @@
-package GurkhaYouthsTrainingCenter.Routine_to_do_management.service;
-
-import GurkhaYouthsTrainingCenter.Routine_to_do_management.dto.TodoDto;
-
-public interface TodoService {
-    TodoDto addTodo(TodoDto todoDto);
-}
