5a42c181e4b271953205f833bbfe53ff7d483bd9
Bhawani1200 <bhawanilimbu1200@gmail.com>
2025-03-06 14:03:39 +0545

    changed

--- Routine-to-do-management/src/main/java/GurkhaYouthsTrainingCenter/Routine_to_do_management/repository/TodoRespository.java
+++
@@ -1,7 +0,0 @@
-package GurkhaYouthsTrainingCenter.Routine_to_do_management.repository;
-
-import GurkhaYouthsTrainingCenter.Routine_to_do_management.entity.Todo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodoRespository extends JpaRepository<Todo,Long> {
-}
