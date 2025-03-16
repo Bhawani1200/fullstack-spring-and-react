bfd61d0a0778893f693e5350e8c2601a9728ad7d
Bhawani1200 <bhawanilimbu1200@gmail.com>
2025-03-16 13:44:51 +0545

    savetodo rest api

--- Routine-todo-management/todoroutine-ui/src/components/TodoComponent.jsx
+++ Routine-todo-management/todoroutine-ui/src/components/TodoComponent.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import { saveTodo, updateTodo } from "../services/TodoServices";
 
 const TodoComponent = () => {
   const [completed, setCompleted] = useState("false");
   const [title, setTitle] = useState();
   const [description, setDescription] = useState();
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
   const id = useParams();
 
   function pageTitle() {
@@ -15,49 +16,32 @@ const TodoComponent = () => {
       return <h2 className="text-center">Add Todo</h2>;
     }
   }
+  function saveOrUpdateTodo(e) {
+    e.preventDefault();
+    const todo = { title, description, completed };
+    console.log(todo);
+
+    if (id) {
+      updateTodo(id, todo)
+        .then((response) => {
+          console.log(response.data);
+          navigate("/todos");
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    } else {
+      saveTodo(todo)
+        .then((response) => {
+          console.log(response.data);
+          navigate("/todos");
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+  }
   return (
-    // <div className="container">
-    //   <div className="row">
-    //     <div className="card col-md-6 offset-md-3 offset-md-3">
-    //       {pageTitle()}
-    //       <div className="card-body">
-    //         <form>
-    //           <div className="form-group mb-2">
-    //             <label htmlFor="">Todo List</label>
-    //             <input
-    //               type="text"
-    //               placeholder="Enter Todo Title"
-    //               name="title"
-    //               value={title}
-    //               onChange={(e) => setTitle(e.target.value)}
-    //             ></input>
-    //           </div>
-    //           <div className="form-group mb-2">
-    //             <label htmlFor="">Todo Description</label>
-    //             <input
-    //               type="text"
-    //               placeholder="Enter Todo Description"
-    //               name="description"
-    //               value={description}
-    //               onChange={(e) => setDescription(e.target.value)}
-    //             ></input>
-    //           </div>
-    //           <div className="form-group mb-2">
-    //             <label htmlFor="">Todo Completed:</label>
-    //             <select
-    //               className="form-control"
-    //               value={completed}
-    //               onChange={(e) => setCompleted(e.target.value)}
-    //             >
-    //               <option value="false">No</option>
-    //               <option value="true">Yes</option>
-    //             </select>
-    //           </div>
-    //         </form>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
     <div className="container">
       <br /> <br />
       <div className="row">
@@ -100,6 +84,12 @@ const TodoComponent = () => {
                   <option value="true">Yes</option>
                 </select>
               </div>
+              <button
+                className="btn btn-success"
+                onClick={(e) => saveOrUpdateTodo(e)}
+              >
+                Submit
+              </button>
             </form>
           </div>
         </div>
--- Routine-todo-management/todoroutine-ui/src/services/TodoServices.js
+++ Routine-todo-management/todoroutine-ui/src/services/TodoServices.js
@@ -1,5 +1,11 @@
 import axios from "axios";
-const BASE_REST_API_URL = 'http://localhost:8080/api/todos';
+const BASE_REST_API_URL = "http://localhost:8080/api/todos";
 
 export const getAllTodos = () => axios.get(BASE_REST_API_URL);
 
+export const saveTodo = (todo) => axios.post(BASE_REST_API_URL, todo);
+
+export const getTodo = (id) => axios.get(BASE_REST_API_URL, +"/" + id);
+
+export const updateTodo = (id, todo) =>
+  axios.put(BASE_REST_API_URL, +"/" + id, todo);
