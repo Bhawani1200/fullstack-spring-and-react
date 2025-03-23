8044868722ca25786ea9a44014f3bf94d88cf915
Bhawani1200 <bhawanilimbu1200@gmail.com>
2025-03-23 13:57:32 +0545

    complete and incomplete rest api

--- Routine-todo-management/todoroutine-ui/src/components/ListTodoComponent.jsx
+++ Routine-todo-management/todoroutine-ui/src/components/ListTodoComponent.jsx
@@ -1,5 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { deleteTodo, getAllTodos } from "../services/TodoServices.js";
+import {
+  completeTodo,
+  deleteTodo,
+  getAllTodos,
+  inCompleteTodo,
+} from "../services/TodoServices.js";
 import { useNavigate } from "react-router-dom";
 
 const ListTodoComponent = () => {
@@ -39,6 +44,26 @@ const ListTodoComponent = () => {
       });
   }
 
+  function markCompleteTodo(id) {
+    completeTodo(id)
+      .then(() => {
+        listTodos();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  function markInCompleteTodo(id) {
+    inCompleteTodo(id)
+      .then(() => {
+        listTodos();
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
   return (
     <div className="container">
       <h2 className="text-center">List of Todos</h2>
@@ -71,9 +96,23 @@ const ListTodoComponent = () => {
                   <button
                     className="btn btn-danger"
                     onClick={() => removeTodo(todo.id)}
-                    style={{marginLeft: "10px"}}
+                    style={{ marginLeft: "10px" }}
                   >
                     Delete
+                  </button>
+                  <button
+                    className="btn btn-success"
+                    onClick={() => markCompleteTodo(todo.id)}
+                    style={{ marginLeft: "10px" }}
+                  >
+                    Complete
+                  </button>
+                  <button
+                    className="btn btn-info"
+                    onClick={() => markInCompleteTodo(todo.id)}
+                    style={{ marginLeft: "10px" }}
+                  >
+                    InComplete
                   </button>
                 </td>
               </tr>
